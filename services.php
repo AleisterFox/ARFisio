00bce439daddc5f03f04f3a392dbd2d871d2bdd1
Interlab Interlab <ericaguirrez@hotmail.com>
2023-11-01 13:19:56 -0600

    changes in services page

--- services.php
+++ services.php
@@ -43,7 +43,7 @@
                     <li class="serv s2">Fisioterapia Ortopédica</li>
                     <li class="serv s3">Fisioterapia Deportiva</li>
                     <li class="serv s4">Readaptación Deportiva</li>
-                    <li class="serv s5">Asesoría Posquirúrgica </li>
+                    <li class="serv s5">Asesoría Posquirúrgica/Via Remota</li>
                     <li class="serv s6">Descarga Muscular</li>
                 </ul>
 
@@ -60,38 +60,38 @@
                             Protesis (rodilla-cadera-hombro) <br> - Cirugias de columna</p>
                         <div class="gallery fadeIn wow">
                             <div>
-                                <img src="images/us2.png" alt="">
+                                <img src="images/electro2.jpg" alt="">
                             </div>
                             <div>
-                                <img src="images/us2.png" alt="">
+                                <img src="images/ondas.jpg" alt="">
                                 <div class="frame"></div>
                             </div>
                         </div>
-                        <h2>Los beneficios de la fisioterapia en la rehabilitación.</h2>
+                        <!-- <h2>Los beneficios de la fisioterapia en la rehabilitación.</h2>
                         <p>Cuando ha habido una lesión o una operación, los músculos o articulaciones implicadas,
                             pierden movilidad y flexibilidad. Depende de la gravedad, será una perdida total o parcial,
                             y para recuperarlas, es necesario tener paciencia y avanzar poco a poco con el tratamiento
                             de rehabilitación seleccionado. <br> La importancia de ponerse en manos de un profesional es
                             decisiva, él sabrá que técnicas son las más adecuadas para nuestra pronta recuperación.
                             Según su capacidad de observación y análisis, decidirá según las características de cada
-                            persona, lo que es imprescindible para su preciso diagnóstico.</p>
+                            persona, lo que es imprescindible para su preciso diagnóstico.</p> -->
                     </div>
                     <div class=" desc s2 hidden">
                         <img src="images/frame-1.png" alt="">
-                        <p>Area especializada en el tratamiento de lesiones neuro-musculo-esqueleticas . Algunas de las
+                        <p>Area especializada en el tratamiento de lesiones neuro-musculo-esqueleticas . <br> Algunas de las
                             patologias que mas tratamos son: <br> - Lesiones ligamentarias ( esguinces ) <br> - Lesiones
                             musculares <br> - Lesiones de cartilago <br> - Fracturas <br> - Luxaciones <br> - Patologia
                             de columna</p>
-                        <div class="gallery fadeIn wow">
+                        <div class="gallery fadeIn">
                             <div>
-                                <img src="images/s2_1.jpg" alt="">
+                                <img src="images/ortopedica.jpg" alt="Fisioterapeuta tratando paciente">
                             </div>
                             <div>
-                                <img src="images/s2_2.jpg" alt="">
+                                <img src="images/trt2.jpg" alt="Fisioterapeuta tratando hombro de paciente">
                                 <div class="frame"></div>
                             </div>
                         </div>
-                        <h2>Preparación para la fisioterapia manual ortopédica</h2>
+                        <!-- <h2>Preparación para la fisioterapia manual ortopédica</h2>
                         <p>Previamente a las intervenciones de Terapia manual ortopédica se realizará una evaluación,
                             para diferenciar las situaciones clínicas en que dichas técnicas están indicadas o
                             contraindicadas, o aquellas que requieren precauciones especiales. También se estudian los
@@ -99,7 +99,7 @@
                             todos los procedimientos de la Terapia manual ortopédica. <br> Asimismo, también se
                             estudiará previamente si en la situación del paciente se puede recibir tratamiento enfocado
                             a disminuir los síntomas o si, por el contrario, será para mejorar la función del paciente.
-                        </p>
+                        </p> -->
                     </div>
                     <div class=" desc s3 hidden">
                         <img src="images/frame.png" alt="">
@@ -108,16 +108,16 @@
                             tratamos son : <br> - Tendinopatia Aquilea <br> - Tendinopatia de Manguito Rotador brq -
                             Fascitis Plantar <br> - Síndrome de la Banda Iliotibial <br> - Rodilla del saltador <br> -
                             Lesiones musculares (desgarres) <br> - Lesiones ligamentarias ( esguinces )</p>
-                        <div class="gallery fadeIn wow">
+                        <div class="gallery fadeIn">
                             <div>
-                                <img src="images/s3_1.png" alt="">
+                                <img src="images/deportiva.jpg" alt="Fisioterapeuta y paciente en rehabilitacion">
                             </div>
                             <div>
-                                <img src="images/s3_2.jpg" alt="">
+                                <img src="images/deportiva3.jpg" alt="Fisioterapeuta y paciente en rehabilitacion">
                                 <div class="frame"></div>
                             </div>
                         </div>
-                        <h2>En qué consiste la fisioterapia deportiva</h2>
+                        <!-- <h2>En qué consiste la fisioterapia deportiva</h2>
                         <p>Consiste en mejorar y trabajar la condición muscular del deportista, evitando las cargas y
                             corrigiendo el mal gesto deportivo. Trabajaremos la musculatura con masaje deportivo para
                             descargar o tonificar según se encuentre en ese momento el deportista. No es igual el
@@ -129,52 +129,42 @@
                             son múltiples, principalmente evitamos el riesgo de lesión, mejora el rendimiento deportivo,
                             la musculatura estará en su mas alto nivel, aumentará su elasticidad, su adaptación al
                             esfuerzo será mejor y por supuesto mejorará su estado anímico y psicológico como ya se ha
-                            demostrado en diferentes estudios.</p>
+                            demostrado en diferentes estudios.</p> -->
                     </div>
                     <div class=" desc s4 hidden">
                         <img src="images/frame.png" alt="">
-                        <p>Somos una clinica de Alta Especialidad en tratamientos Post-quirurgicos. Contamos con
-                            especialistas en el ramo de la Fisioterapia Ortopedica-Traumatologica. <br> Nuestros
-                            tratamientos tienen su base, en los protocolos mas avanzados de rehabilitacion; y nuestra
-                            tecnologia, nos permite seguir paso a paso dicho tratamiento. Algunos de los tratameintos
-                            Post-quirurgicos mas comunes que vemos son : <br> LCA ( ligamento cruzado anterior ) <br> -
-                            Mensicoplastias <br> - Condroplastias <br> - Fracturas con tratamientos quirurgicos <br> -
-                            Tendon de Aquiles <br> - Manguito Rotador <br> - Inestabilidad Glenohumeral <br> - Protesis
-                            (rodilla-cadera-hombro) <br> - Cirugias de columna</p>
-                        <div class="gallery fadeIn wow">
+                        <p>Servicio que proporcionamos a aquellos pacientes , que han tenido una lesion, y desean regresar a practicar su actividad deportiva favorita. <br> <br> Consiste en un programa , basado en ejercicio terapeutico, adaptado e individualizado , a los objetivos de cada paciente y patologia.</p>
+                        <div class="gallery fadeIn">
                             <div>
-                                <img src="images/s2_1.jpg" alt="">
+                                <img src="images/icaros2.jpg" alt="Rehabilitación con simulador virtual">
                             </div>
                             <div>
-                                <img src="images/s2_2.jpg" alt="">
+                                <img src="images/deportiva4.jpg" alt="Fisioterapeuta y paciente en rehabilitacion">
                                 <div class="frame"></div>
                             </div>
                         </div>
-                        <h2>Los beneficios de la fisioterapia en la rehabilitación.</h2>
+                        <!-- <h2>Los beneficios de la fisioterapia en la rehabilitación.</h2>
                         <p>Cuando ha habido una lesión o una operación, los músculos o articulaciones implicadas,
                             pierden movilidad y flexibilidad. Depende de la gravedad, será una perdida total o parcial,
                             y para recuperarlas, es necesario tener paciencia y avanzar poco a poco con el tratamiento
                             de rehabilitación seleccionado. <br> La importancia de ponerse en manos de un profesional es
                             decisiva, él sabrá que técnicas son las más adecuadas para nuestra pronta recuperación.
                             Según su capacidad de observación y análisis, decidirá según las características de cada
-                            persona, lo que es imprescindible para su preciso diagnóstico.</p>
+                            persona, lo que es imprescindible para su preciso diagnóstico.</p> -->
                     </div>
                     <div class=" desc s5 hidden">
                         <img src="images/frame-1.png" alt="">
-                        <p>Area especializada en el tratamiento de lesiones neuro-musculo-esqueleticas . Algunas de las
-                            patologias que mas tratamos son: <br> - Lesiones ligamentarias ( esguinces ) <br> - Lesiones
-                            musculares <br> - Lesiones de cartilago <br> - Fracturas <br> - Luxaciones <br> - Patologia
-                            de columna</p>
-                        <div class="gallery fadeIn wow">
+                        <p>Consiste en una sesion online , con uno de nuestros profesionales expertos en Rehabilitacion Postquirurgica. Donde buscamos evaluar cada caso , y orientar al paciente en su proceso de Rehabilitacion. <br> Duracion de la sesion : 30 min </p>
+                        <div class="gallery fadeIn">
                             <div>
-                                <img src="images/s2_1.jpg" alt="">
+                                <img src="images/game.jpg" alt="">
                             </div>
                             <div>
-                                <img src="images/s2_2.jpg" alt="">
+                                <img src="images/fibrolisis.jpg" alt="">
                                 <div class="frame"></div>
                             </div>
                         </div>
-                        <h2>Preparación para la fisioterapia manual ortopédica</h2>
+                        <!-- <h2>Preparación para la fisioterapia manual ortopédica</h2>
                         <p>Previamente a las intervenciones de Terapia manual ortopédica se realizará una evaluación,
                             para diferenciar las situaciones clínicas en que dichas técnicas están indicadas o
                             contraindicadas, o aquellas que requieren precauciones especiales. También se estudian los
@@ -182,23 +172,20 @@
                             todos los procedimientos de la Terapia manual ortopédica. <br> Asimismo, también se
                             estudiará previamente si en la situación del paciente se puede recibir tratamiento enfocado
                             a disminuir los síntomas o si, por el contrario, será para mejorar la función del paciente.
-                        </p>
+                        </p> -->
                     </div>
                     <div class=" desc s6 hidden">
                         <img src="images/frame-2.png" alt="">
-                        <p>Área dedicada al tratamiento y prevención de lesiones en el ámbito deportivo; tanto en
-                            deportistas amateurs, de elite o profesionales. <br> Algunas de las patologías que mas
-                            tratamos son : <br> - Tendinopatia Aquilea <br> - Tendinopatia de Manguito Rotador brq -
-                            Fascitis Plantar <br> - Síndrome de la Banda Iliotibial <br> - Rodilla del saltador <br> -
+                        <p>Servicio que se realiza a deportistas , antes o despues de tener una actividad fisica importante . Se realiza por un profesional calificado. <br> Pueden aplicarse diferentes tecnicas , como: <br> - Masaje <br> - Presoterapia <br> - Electroterapia <br> - Percusiónl
                             Lesiones musculares (desgarres) <br> - Lesiones ligamentarias ( esguinces )</p>
-                        <div class="gallery fadeIn wow">
-                            <div><img src="images/s2_1.jpg" alt=""></div>
+                        <div class="gallery fadeIn">
+                            <div><img src="images/presoterapia.jpg" alt=""></div>
                             <div>
-                                <img src="images/s2_2.jpg" alt="">
+                                <img src="images/masaje.jpg" alt="">
                                 <div class="frame"></div>
                             </div>
                         </div>
-                        <h2>En qué consiste la fisioterapia deportiva</h2>
+                        <!-- <h2>En qué consiste la fisioterapia deportiva</h2>
                         <p>Consiste en mejorar y trabajar la condición muscular del deportista, evitando las cargas y
                             corrigiendo el mal gesto deportivo. Trabajaremos la musculatura con masaje deportivo para
                             descargar o tonificar según se encuentre en ese momento el deportista. No es igual el
@@ -210,7 +197,7 @@
                             son múltiples, principalmente evitamos el riesgo de lesión, mejora el rendimiento deportivo,
                             la musculatura estará en su mas alto nivel, aumentará su elasticidad, su adaptación al
                             esfuerzo será mejor y por supuesto mejorará su estado anímico y psicológico como ya se ha
-                            demostrado en diferentes estudios.</p>
+                            demostrado en diferentes estudios.</p> -->
                     </div>
                 </div>
             </div>
@@ -227,7 +214,7 @@
                 </div>
                 <div class="phone">
                     <img src="images/phone.png" alt="icono de telefono">
-                    <p>553 693 37 39</p>
+                    <p>55 83 73 73 95</p>
                 </div>
             </div>
 
